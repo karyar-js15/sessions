d3fe653fdef8faf967f6a09868ab61d3a48cb33c
Hadi Tabatabaei <hadi.tabatabaei@divar.ir>
2026-06-05 12:05:29 +0330

    feat: Add sessions 1 and 2

---
+++ s1/codes/anothercodes/index.js
@@ -0,0 +1,53 @@
+// LS ~ DIR
+// CD
+// CLEAR ~ CLS
+// MKDIR
+// RMDIR
+// RM
+// TOUCH
+// &&
+
+// Arguments - آرگومان
+// man ~ Manual
+
+
+/**
+ * 
+ * Register - ثبات - ثبت کننده
+ * 
+ * Cache - (۵۰ مگابایت)
+ * 
+ * Memory: RAM - حافظه
+ *  محل ذخیره کردن داده
+ *  موقت - Volatile =>‌ با خاموش شدن کامپیوتر، میپره
+ *  وقتی که، نرم افزاری بسته می‌شه، حافظه‌ای که مصرف کرده آزاد می‌شه
+ *  quota: 4Gigabye, 8Gigabyte, 16Gigabyte, 32Gigabyte
+ *  300Gigabyte RAM ?
+ *  8 Gigabyte => 20,000,000
+ * گران - حجم نسبتا کم - موقت - ارتباط باهاش سریع
+ * 
+ * Drive (Solid State, Hard Disk) - درایو - دیسک
+ * دائمی - Permanent
+ * حجمش زیاده - مثلا 1000 گیگ
+ * کند تر
+ * ارزون تره
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ *  ثابت - Persistent => 
+ *  ارتباطی با نرم افزارهای باز روی سیستم داره
+ */
+
+
+/**
+ * Interl Core i5 15400 64bit
+ * 100 دستور - Instruction (Instruction Set)
+ * ADD 
+ * Frequency ? 2.5Ghz 2.5 میلیارد یه چیزی در ثانیه
+ * 
+ * Operating System - سیستم عامل - OS
+ * Windows, MacOS, Linux
+ */
---
+++ s2/script.js
@@ -0,0 +1,100 @@
+// console.log("Hello World")
+// Logic - منطق
+// مدیریت داده
+    // Input: یک یا چند تا داده از کاربر ورودی بگیرم 
+    // Process: یک یا چند عملیات روی داده انجام بدم و خروجی بگیرم
+    // Output: نمایش خروجی به کاربر
+
+// number (عدد) 
+// string (رشته) - تعدادی کاراکتر پشت سر هم
+// boolean - true | false
+// array - (آرایه) - لیستی از داده‌هاست
+// object - (شیٔ) - یک نوع داده‌ی تجمیع شده از بقیه‌ داده هاست
+// null - null
+// undefined - undefined
+// BigInt (اعداد بزرگ)
+
+3874238
+3_874_238
+1.257
+0.3984
+-1024
++1834
+Infinity
+-Infinity
+NaN
+
+// 000 (3bit)
+// 000 001 010 011 100 101 110 111
+// 0   1   2   3   4   5   6   7
+// +0  +1  +2  +3  -0  -1  -2  -3
+// IEEE 754 - استاندارد پترن بیتی اعداد اعشاری ۳۲ بیتی و ۶۴ بیتی
+
+"jef8ur838f'djnu23hd 3y2eyh'd73hd + 283h'dbdn - /dwij2"
+'fpoekw"k329fj'
+`fcowejfn1`
+""
+''
+``
+"12345"
+12345
+
+true
+false
+
+"true"
+true
+
+"false"
+false
+
+[1, false, "ifhjwuefh", -234, "diji3dj", true]
+
+"null"
+null
+
+"undefined"
+undefined
+
+// Type Casting - تبدیل تایپ‌های داده‌ها به یکدیگر
+    // دیتایی از جنس رشته داشته باشم، با تبدیل دیتا به نامبر، چه اتفاقی میفته
+    // Coercion
+
+// Dynamic Typing (تایپ پویا)
+// int a = 1;
+
+// number ~ 
+// 
+// int (integer) + 
+// float (عدد اعشاری) + 
+// double (عدد اعشاری با دقت بالاتر) + 
+// unsigned int (uint) عدد صحیح بدون علامت
+// 
+
+// string ~
+    // string
+    // char 
+    // 
+
+// array
+    // touple (لیست دوتایی - جفت)
+    // ArrayList
+    // List
+    // LinkedList
+
+// Variable Declaration - تعریف متغیر
+// Data Types (Type) - نوع داده‌ها
+// Statement 
+    // Conditional Statement - عبارات شرطی
+    // Loop - حلقه‌ها
+// Operation - عملیات‌ها
+    // Arithmetic Operation - عملیات ریاضی
+    // Logical Operation - عملیات منطقی
+
+let sum = 0;
+
+for(let i = 0; i <= 1000; i++) {
+    sum += i;
+}
+
+console.log(sum)
