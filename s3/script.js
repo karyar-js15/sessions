50bef3430c570bcd22957ecc60d192f96daf9830
Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2026-06-06 20:49:49 +0330

    feat: Add session 3

---
+++ s3/script.js
@@ -0,0 +1,158 @@
+// variable 
+// RAM
+
+// 
+// let, const, var
+// Variable Declaration - تعریف متغیر
+let age;
+// Variable Assignment
+age = 30;
+
+// Re-Declare 
+// let age; // SynaxError: Identifier 'age' has already been declared
+
+// X Starting with number
+// let 20age = 5;
+
+// X no whitespace in variable
+// let a b = 5;
+
+// let some20Variable = 50
+
+// let anotherAge = 35;
+
+// age = 'salam'
+
+
+
+// snake_case
+// camelCase lowerCamelCase
+// CamelCase UpperCamelCase PascalCase
+// kebab-case
+// SCREAMING_SNAKE_CASE
+
+// let my_age = 20;
+// let nationalId = '012345'
+// let NationalxId = '398435543';
+// let national-id = '982345' This convention causes error
+// let MAXIMUM_VALID_AGE = 60
+// let VALID_BRAND = 'apple'
+// let VALID_BRANDS = ['apple', 'google', 'xiaomi']
+// VALID_BRANDS = 'fiwefjhwo'
+
+
+
+
+/* string number boolean array */
+                          // 6 000 000
+let firstName; // string  // 5 673 145 0xFF267341
+let lastName; // string
+let nationalId; // string
+let age // number
+let parent // string
+
+
+let firstName = 'fejwfhwejf'
+
+console.log(firstName)
+console.log('fejwfhwejf')
+
+/*
+    نام           string string string (first_name, name, firstName) 
+    نام خانوادگی  string (familyName, lastName, lastname, LastName)
+    شماره ملی.    string string 
+    سن.           number 
+    نام پدر.      string
+*/
+// Abstraction - انتزاعی کردن
+
+// base64 -> یه گونی کاراکتر
+// 0 1 "010101010100010101010010101010110"
+// "(130,200,0)()()()()()()()()()()()()"
+// Encode کردن - Encoding - کدگزاری کردن
+// rgb(0, 255)
+// red: 0-255
+// blue: 0-255
+// green: 0-255
+// Bank Account:
+/*
+    موجودی حساب : balance // number
+    نام و نام خانوادگی صاحب حساب
+    کد ملی صاحب حساب
+    شماره حساب  : accountNumber: string
+    شماره کارت وابسته به حساب: cardNumber: string
+*/
+
+
+
+// 01010101000001011110
+// 8 GigaByte
+// 
+
+// 8 000 000 000 
+/*
+
+
+
+*/
+
+
+// Primitive - Call by Value
+/*
+    boolean
+    string
+    number
+    null
+    undefined
+*/
+let age = 20;
+let age2 = 20;
+age = 30
+
+let myNumbers = []
+let myNumbers2 = myNumbers;
+
+// Clone (کپی کردن)
+
+let myPrim = "salam"
+let myPrim2 = myPrim
+
+
+
+/*
+    var1 => 0xEEE(0xFFF) => 0xFFF([])
+*/
+
+
+// let myNumbers2 = []
+
+// Complex (Non-Primitive) - Call By Reference
+/*
+    array
+    object
+*/
+
+
+
+/* 
+    Next Session: 
+    Statement (Syntax)
+    Semantic VS Syntax
+    Operator
+        Arithmetic Operator
+        Logical Operator
+        String Operator
+        Precedence (ترتیب/اولویت)
+        1 + 2 * 3 // 7
+    Conditional Statement (if, if else, if else if)
+*/
+
+
+
+
+
+// if(age <= 50) {
+
+// }
+
+// age - 50
