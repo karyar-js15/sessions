aede7e8daa8d8f3e50b1e31027467c5efd550c67
Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2026-08-15 20:36:17 +0330

    feat

---
+++ s21/script.js
@@ -0,0 +1,88 @@
+// Introduction to Network
+
+// Deeper Layer - Physical Layer
+// کابل مسی - فیبر نوری - موج مغناطیسی
+
+// Inetnet Protocol - IP
+// Data Packet (Origin, Data, Header, Target)
+// IP Address 
+// 0-255.0-255.0-255.0-255 (IP Address V4)
+// 0.0.0.1 
+// 0.0.0.2
+// 0.0.0.3
+
+// Network Layer (TCP, UDP)
+
+// Application Layer - Protocol (FTP, HTTPS, HTTP, SSH)
+
+
+
+// Driver Buletooth
+2 ^ 10
+// Private IP Address, Public IP Address
+
+
+
+// IP Adress
+// Port 65500 - 1000, 
+
+// Digikala: 72.20.15.2
+// Divar: 
+// Google:
+// Bale: 
+// Telegram:
+
+// Domain Name, 
+// digikala.com
+// divar.ir
+// google.com
+// bale.ai
+
+// DNS - Domain Name Service - Kill Switch
+
+// digikala.com => 0.0.0.0
+// divar.ir => 10.20.30.40
+
+
+
+// HTTP:
+// URL => Uniform Resource Locator
+
+// file://
+// http://
+// ftp://
+// ssh://
+// https://hadi:password@domain.com:80/a/b/c/d/e
+// https://hadi:password@domain.com:80/a/b/c/d/e?name=hadi&age=20&foo=bar#start
+// https://divar.ir/s/tehran/car
+
+
+// https://upload.wikimedia.org/wikipedia/commons/d/d6/URI_syntax_diagram.svg?utm_source=en.wikipedia.org&utm_campaign=index&utm_content=original
+// https://a.b/837295
+
+// Scheme(Protocol) HTTPS
+// Host (Domain OR IP Addr) upload.wikimedia.org
+// Path /wikipedia/commons/d/d6/URI_syntax_diagram.svg
+// UserInfo -
+// Port -
+// Query ?utm_source=en.wikipedia.org&utm_campaign=index&utm_content=original
+// Fragment -
+
+
+/*
+    HTTP Protocol
+    Hyper Text Transfer Protocol
+    // HTTP Request => Target => HTTP Response
+
+    Request:
+    - URL
+    - Method (GET, POST, DELETE, PUT, PATCH, DELETE, HEAD, ...)
+    - Headers 
+    - Body(Data)
+
+    Response:
+    - Status Code (200, 204, 201, 301, 302, 400, 404, 405, 500, 502, ...)
+    - Status
+    - Headers
+    - Body(Data)
+*/
